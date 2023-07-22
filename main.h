ba813184b493dee402c3af82d6f0cf3c7bac428c
Nasef161995 <mnasef905@gmail.com>
2023-07-22 15:22:17 -0700

    printf task

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+/* Define STRING */
+#define STRING ((str) ? str : "(null)")
+/* Create the functions  */
+int _printf(const char *format, ...);
+int _my_putchar(char c);
+int _my_putstr(char *s);
+void _my_putint(int n);
+int _num_length(int n);
+int _check(char c, va_list args);
+int _myprint_binary(unsigned int n);
+
+#endif
